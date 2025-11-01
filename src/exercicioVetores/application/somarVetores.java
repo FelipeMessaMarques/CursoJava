5a393bcd123a8f6f28ac3314b9d0b36dea7df9a0
FelipeMessaMarques <felipembmarques@gmail.com>
2025-11-01 19:46:43 -0300

    toda lista de exercicios sobre vetores

---
+++ src/exercicioVetores/application/somarVetores.java
@@ -0,0 +1,40 @@
+package exercicioVetores.application;
+
+import java.util.Scanner;
+
+public class somarVetores {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe quantos números você quer informar em cada conjunto: ");
+        int n = scan.nextInt();
+
+        double[] vetorA = new double[n];
+        double[] vetorB = new double[n];
+        double[] vetorC = new double[n];
+
+        System.out.println("Preenchimento do conjunto A.");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println("Digite um número: ");
+            vetorA[i] = scan.nextDouble();
+        }
+
+        System.out.println("Preenchimento do conjunto B.");
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.println("Digite um número: ");
+            vetorB[i] = scan.nextDouble();
+        }
+
+        for (int i = 0; i < vetorC.length; i++) {
+            vetorC[i] = vetorA[i] + vetorB[i];
+        }
+
+        System.out.println("VETOR RESULTANTE: ");
+        for (int i = 0; i < vetorC.length; i++) {
+            System.out.printf("%.2f\n", vetorC[i]);
+        }
+
+        scan.close();
+    }
+}
