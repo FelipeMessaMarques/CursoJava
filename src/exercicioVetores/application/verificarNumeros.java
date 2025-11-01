2302a1b045adf926af4c2cea96e62266ed2b02ad
felipe <felipembmarques@gmail.com>
2025-11-01 07:25:39 -0300

    novas atividades sobre vetores

---
+++ src/exercicioVetores/application/verificarNumeros.java
@@ -0,0 +1,37 @@
+package exercicioVetores.application;
+
+import java.util.Scanner;
+
+public class verificarNumeros {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Quantos números você vai digitar?");
+        int n = scan.nextInt();
+
+        while (n > 10) {
+            System.out.println("Pode ser informado apenas 10 números.");
+            System.out.println("Quantos números você vai digitar?");
+            n = scan.nextInt();
+        }
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.println("Digite um número: ");
+            vect[i] =  scan.nextDouble();
+        }
+
+        System.out.println("Números negativos: ");
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] < 0) {
+                System.out.printf("%.2f\n", vect[i]);
+            }
+        }
+
+        scan.close();
+    }
+
+}
